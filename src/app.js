416f2171389a1acc434d0d4e8f8d10c0312fd2ad
tgrier6 <tiaragrier2@gmail.com>
2021-04-05 18:04:46 -0400

    Added intial JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 alert(`Hello!`);
+
+//Day & Time
+/*function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Staurday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} | ${hours}:${minutes}`;
+}*/
