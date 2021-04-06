8513ea65d436a1a6f3b812f470a2b91159609acb
tgrier6 <tiaragrier2@gmail.com>
2021-04-06 15:30:36 -0400

    Updated weather, day, and time JS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-alert(`Hello!`);
-
 //Day & Time
-/*function formatDate(date) {
+function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -24,4 +22,53 @@ alert(`Hello!`);
   let day = days[dayIndex];
 
   return `${day} | ${hours}:${minutes}`;
-}*/
+}
+
+//Weather & Location
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "98b5711bc7358d439ba8e0b45dbf74b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "98b5711bc7358d439ba8e0b45dbf74b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchBar = document.querySelector("#city-input");
+searchBar.addEventListener("submit", handleSubmit);
+
+let searchButton = document.querySelector("#btn");
+searchButton.addEventListener("click", handleSubmit);
+
+let currentLocationButton = document.querySelector("#arrow-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Atlanta, GA");
