d8b2e571efbaa2f45aa029cf00af89b06cc76718
tgrier6 <tiaragrier2@gmail.com>
2021-04-07 14:10:49 -0400

    ...http...

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ function displayWeatherCondition(response) {
     response.data.main.temp
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = math.round(
+  document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
