62c05c6cd6b2e1af5090bb30ca26ed7377bea625
tgrier6 <tiaragrier2@gmail.com>
2021-04-08 15:58:41 -0400

    ...

--- src/app.js
+++ src/app.js
@@ -111,7 +111,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `<div class="col-3">
