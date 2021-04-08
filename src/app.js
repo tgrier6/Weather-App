1139aff160465ec6091c704d3f5dd252131a3f2d
tgrier6 <tiaragrier2@gmail.com>
2021-04-08 15:51:36 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,6 +24,14 @@ function formatDate(date) {
   return `${day} | ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 //Weather & Location
 function getForecast(coordinates) {
   console.log(coordinates);
@@ -44,6 +52,7 @@ function displayWeatherCondition(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
 
+  //Icon
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -100,23 +109,31 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-3">
-          <div class="weather-forecast-date">${forecastDay.dt}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-3">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
             <img
-            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             class="forecast-icons"
             />
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">${forecastDay.temp.max}˚ |</span>
-              <span class="weather-forecast-temperature-min">${forecastDay.temp.min}˚</span>
+              <span class="weather-forecast-temperature-max">${Math.round(
+                forecastDay.temp.max
+              )}˚ |</span>
+              <span class="weather-forecast-temperature-min">${Math.round(
+                forecastDay.temp.min
+              )}˚</span>
             </div>
         </div>
         </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div> `;
