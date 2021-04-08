c03b7ac433e60bed674cd608233abcba156a94db
tgrier6 <tiaragrier2@gmail.com>
2021-04-08 12:53:32 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -89,27 +89,26 @@ displayForecast();
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `<div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-            <img
-            src="http://openweathermap.org/img/wn/03d@2x.png"
-            alt=""
-            width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">70˚</span>
-              <span class="weather-forecast-temperature-min">55˚</span>
-            </div>
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
-    </div>
-    `;
+      </div>
+  `;
   });
 
-  forecastHTML = forecastHTML + `</div> `;
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-}
