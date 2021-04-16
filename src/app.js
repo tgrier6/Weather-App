ac6a362d48fdee28227587dbafea19351f22980c
tgrier6 <tiaragrier2@gmail.com>
2021-04-15 20:50:31 -0400

    moving forecast div

--- src/app.js
+++ src/app.js
@@ -109,7 +109,7 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div>`;
+  let forecastHTML = `<div class="row" id="float-container">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
